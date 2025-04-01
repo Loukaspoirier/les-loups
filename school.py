336b93dbae491582153ae26c63f3419a188fba52
argonaultes <gael@argonaultes.fr>
2025-04-01 14:38:10 +0200

    fix school abstract

--- school.py
+++ school.py
@@ -1,4 +1,6 @@
-class School:
+from abc import ABC, abstractmethod
+
+class School(ABC):
     
     def __init__(self, name):
         self.__name = name # dunder
@@ -11,13 +13,33 @@ class School:
     def get_name(self):
         return self.__name
 
+    @abstractmethod
+    def subscribe(self):
+        pass
+
+class Hexagone(School):
+
+    def __init__(self, teacher = None):
+        super().__init__('hexagone')
+
+    def subscribe(self):
+        print('subscribe new student')
+
+class Sqy(School):
+    def __init__(self, teacher = None):
+        super().__init__('sqy')
+
+    def subscribe(self):
+        print('subscribe new student')
+
+
 def use_school():
     hexagone = School('hexagone')
     print(__name__, hexagone, hexagone.get_name())   
 
-hexagone = School('hexagone')
+hexagone = Hexagone()
 print(__name__, hexagone, hexagone.name)
-sqy = School('sqy')
+sqy = Sqy('sqy')
 sqy.students = ['student1', 19] # [] {} () set()
 print(sqy.students)
 #print(hexagone.students)
