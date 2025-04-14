9a0952f4b428700b7e86946d5e7a636778d5b644
enniriss <sirinekhlifi0411@gmail.com>
2025-04-14 11:14:06 +0200

    add folders and wiews

--- wv_schema.sql
+++ wv_schema.sql
@@ -53,3 +53,30 @@ create table players_play (
     target_position_col text,
     target_position_row text
 );
+
+
+----------------------------
+
+-- Ajouter les clés étrangères dans la table roles_quotas
+ALTER TABLE roles_quotas
+ADD CONSTRAINT fk_roles_quotas_party FOREIGN KEY (id_party) REFERENCES parties(id_party),
+ADD CONSTRAINT fk_roles_quotas_role FOREIGN KEY (id_role) REFERENCES roles(id_role);
+
+-- Ajouter les clés étrangères dans la table obstacles
+ALTER TABLE obstacles
+ADD CONSTRAINT fk_obstacles_party FOREIGN KEY (id_party) REFERENCES parties(id_party),
+
+-- Ajouter les clés étrangères dans la table players_in_parties
+ALTER TABLE players_in_parties
+ADD CONSTRAINT fk_players_in_parties_party FOREIGN KEY (id_party) REFERENCES parties(id_party),
+ADD CONSTRAINT fk_players_in_parties_player FOREIGN KEY (id_player) REFERENCES players(id_player),
+ADD CONSTRAINT fk_players_in_parties_role FOREIGN KEY (id_role) REFERENCES roles(id_role);
+
+-- Ajouter les clés étrangères dans la table turns
+ALTER TABLE turns
+ADD CONSTRAINT fk_turns_party FOREIGN KEY (id_party) REFERENCES parties(id_party);
+
+-- Ajouter les clés étrangères dans la table players_play
+ALTER TABLE players_play
+ADD CONSTRAINT fk_players_play_in_parties FOREIGN KEY (id_players_in_parties) REFERENCES players_in_parties(id_players_in_parties),
+ADD CONSTRAINT fk_players_play_turn FOREIGN KEY (id_turn) REFERENCES turns(id_turn);
