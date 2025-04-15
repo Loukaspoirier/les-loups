9367993f97d76954f8c2c5fe08da3724e213dfb8
enniriss <sirinekhlifi0411@gmail.com>
2025-04-15 13:59:44 +0200

    try again

---
+++ moteur-administration/app2.py
@@ -0,0 +1,23 @@
+import psycopg2
+import os
+
+def insert_data():
+    conn = psycopg2.connect(
+        host='::',
+        database='les-loups',
+        user='user',
+        password='password'
+    )
+
+    cur = conn.cursor()
+    
+    # Insertion d'une ligne dans la table users
+    cur.execute("INSERT INTO players (pseudo) VALUES (%s)", ("Sissi"))
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    print("Data inserted successfully")
+
+if __name__ == '__main__':
+    insert_data()
